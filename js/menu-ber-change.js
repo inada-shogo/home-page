99d57e2fd037689df9a1524854a8c8a0827f4f52
inada <inada.s@marietta.co.jp>
2022-02-22 12:46:07 +0900

    [Add]

---
+++ js/menu-ber-change.js
@@ -0,0 +1,6 @@
+$(function () {
+  $('.js-btn').on('click', function () {        // js-btnクラスをクリックすると、
+    $('.menu , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+    $('body').toggleClass('cover');
+  })
+});
---
+++ js/typing.js
@@ -0,0 +1,6 @@
+$('.typing').t({
+  delay: 0.5,
+  speed: 100,
+  speed_vary: false,
+  repeat: false,
+});
