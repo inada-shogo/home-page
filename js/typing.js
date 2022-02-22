1d7875224f70f4e886b08f98d68f83ecc21a7811
inada <inada.s@marietta.co.jp>
2022-02-22 22:23:27 +0900

    [Add]

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,9 @@
-$('.typing').t({
-  delay: 0.5,
-  speed: 100,
-  speed_vary: false,
-  repeat: false,
-});
+setTimeout(() => {
+  $(".typing").addClass("show")
+  $('.typing').t({
+    delay: 0.5,
+    speed: 100,
+    speed_vary: false,
+    repeat: false,
+  });
+}, 1000);
