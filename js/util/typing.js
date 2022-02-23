bf50a1af7fd5c6c9d0134c79c6aac601c9c8734e
inada <inada.s@marietta.co.jp>
2022-02-23 16:23:21 +0900

    [Add]

--- js/util/typing.js
+++ js/util/typing.js
@@ -8,14 +8,3 @@ setTimeout(() => {
     caret:false
   });
 }, 250);
-
-setTimeout(() => {
-  $(".about-text").addClass("js-shows")
-  $('.about-text').t({
-    delay: 0.5,
-    speed: 50,
-    speed_vary: false,
-    repeat: false,
-    caret:false
-  });
-}, 1000);
